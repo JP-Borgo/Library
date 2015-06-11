d0d61d726ef5b667e1eb1a982bc119a5b8a93692
JP-Borgo <juanpabloborgogil@hotmail.com>
2015-06-11 17:49:16 -0600

    Added comment for test

--- Library_Manager/Library_Manager/Form1.cs
+++ Library_Manager/Library_Manager/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//juan comment
 namespace Library_Manager
 {
     public partial class Form1 : Form
