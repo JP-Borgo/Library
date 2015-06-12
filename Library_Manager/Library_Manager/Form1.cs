0b6dabf867aac06ca1374ff2b3e6e9f1575b6cb7
lsteeno <lsteeno@gmail.com>
2015-06-11 18:27:24 -0600

    lances change

--- Library_Manager/Library_Manager/Form1.cs
+++ Library_Manager/Library_Manager/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 //juan comment
 //sntoher comment from Juan Pablo
+//lance jj
 namespace Library_Manager
 {
     public partial class Form1 : Form
